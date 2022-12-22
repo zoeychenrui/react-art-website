ac126fe0001595ca3988c5eaf453e75d076c0098
zoeychenrui <zoeychen2013@gmail.com>
2022-12-22 17:05:18 -0600

    image hover

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom'
 export default function Piece(props) {
   return (
     <figure className="image-item">
-    <Link to={props.path}>
+    <Link className="no-line" to={props.path}>
     <img 
       src={props.src}
       className='img'
-      alt="Piece"
-       />
-    <figcaption class="overlay"><span>{props.text}</span></figcaption>
+      alt="Piece"/>
+    <img 
+      src={props.src2}
+      className='hover-img'
+      alt="Piece"/>
+    <figcaption><span>{props.text}</span></figcaption>
     </Link>
     </figure>
   )
--- src/components/pages/Originals.js
+++ src/components/pages/Originals.js
@@ -10,16 +10,19 @@ export default function Originals () {
             <div className="column">
                 <Piece
                     src='original/worlds.jpg'
+                    src2='original/worlds-one.jpg'
                     text='physical and virtual body-and-worlds'
                     path='/originals'
                     />
                     <Piece
                     src='original/pumpkin-art.jpg'
+                    src2='original/pumpkin-pic.jpg'
                     text='pumpkin'
                     path='/originals'
                     />
                     <Piece
                     src='original/toby-art.jpg'
+                    src2='original/toby-pic.jpg'
                     text='toby'
                     path='/originals'
                     />
@@ -27,16 +30,19 @@ export default function Originals () {
             <div className="column">
                     <Piece
                     src='original/cole-art.jpg'
+                    src2='original/cole-pic.jpg'
                     text='cole'
                     path='/originals'
                     />
                     <Piece
                     src='original/clifford-art.jpg'
+                    src2='original/clifford-pic.jpg'
                     text='clifford'
                     path='/originals'
                     />
                     <Piece
                     src='original/missy-art.jpg'
+                    src2='original/missy-pic.jpg'
                     text='missy'
                     path='/originals'
                     />
@@ -44,11 +50,13 @@ export default function Originals () {
             <div className="column">
                     <Piece
                     src='original/pear-cat-art.jpg'
+                    src2='original/pear-cat-pic.PNG'
                     text='pear cat'
                     path='/originals'
                     />
                     <Piece
                     src='original/computer-cat-art.jpg'
+                    src2='original/computer-cat-pic.jpg'
                     text='computer cat'
                     path='/originals'
                     />
