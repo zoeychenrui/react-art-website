641cdff42fe08b2d1807fb1321fb9d5247a3c10d
zoeychenrui <zoeychen2013@gmail.com>
2022-12-26 00:55:00 -0600

    about

--- src/components/pages/Pinterest.js
+++ src/components/pages/Pinterest.js
@@ -3,12 +3,15 @@ import '../../App.css';
 
 export default function Pinterest () {
 
-    (function(d){  var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');  p.type = 'text/javascript';  p.async = true;  p.src = '//assets.pinterest.com/js/pinit.js';  f.parentNode.insertBefore(p, f);}(document));
+    (function(d)
+    {  var f = d.getElementsByTagName('SCRIPT')[0], 
+    p = d.createElement('SCRIPT');  
+    p.src = '//assets.pinterest.com/js/pinit.js';  
+    f.parentNode.insertBefore(p, f);}(document));
     
     return (
-        <center>
-            <a data-pin-do="embedBoard" data-pin-board-width="900" data-pin-scale-height="700" data-pin-scale-width="200" href="https://www.pinterest.com/zoeychen2013/paint-class/"></a>
-            
+        <center className="container">
+            <a data-pin-do="embedBoard" data-pin-board-width="900" data-pin-scale-height="550" data-pin-scale-width="200" href="https://www.pinterest.com/zoeychen2013/paint-class/"></a>
         </center>
     )
 }
