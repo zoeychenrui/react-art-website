8b5748eff8226cdb7b4d94bb69335de4b6cc4c7e
zoeychenrui <zoeychen2013@gmail.com>
2022-12-26 21:52:37 -0600

    url updates

--- src/components/pages/Teaching.js
+++ src/components/pages/Teaching.js
@@ -13,7 +13,7 @@ export default function Teaching () {
                 <Slider name="pottedCacti/" text="potted cacti 9.13.2020" url="/pottedCacti/"/>
                 <Slider name="assortedFlowers/" text="assorted flowers 8.30.2020" url="/assortedFlowers/"/>
                 <Slider name="goldenGateBridge/" text="golden gate bridge 9.27.2020" url="/goldenGateBridge/"/>
-                <Slider name="paris/" text="paris 2.14.2021" url="/paris"/>
+                <Slider name="paris/" text="paris 2.14.2021" url="/paris/"/>
                 <Slider name="lavendarFarm/" text="lavendar farm 2.28.2021" url="/lavendarFarm/"/>
                 <Slider name="hotAirBalloon/" text="hot air balloon 8.23.2020" url="/hotAirBalloon/"/>
                 <Slider name="desert/" text="desert 8.9.2020" url="/desert/"/>
@@ -27,12 +27,12 @@ export default function Teaching () {
                 <Slider name="sunsetInMorocco/" text="sunset in morocco 1.10.2021" url="/sunsetInMorocco/"/>
                 <Slider name="mountains/" text="mountains 11.8.2020" url="/mountains/"/>
                 <Slider name="lavendar/" text="lavendar 7.5.2020" url="/lavendar/"/>
-                <Slider name="pandaClass/" text="panda 4.5.2020" url="/panda/"/>
-                <Slider name="sunflowersClass/" text="sunflowers 4.26.2020" url="/sunflowers/"/>
-                <Slider name="rainbowSkyClass/" text="rainbow sky 5.24.2020" url="/rainbowSky/"/>
-                <Slider name="fireflyClass/" text="firefly 5.17.2020" url="/firefly/"/>
-                <Slider name="abstractFlowersClass/" text="abstract flowers 6.14.2020" url="/abstractFlowers/"/>
-                <Slider name="flowersClass/" text="flowers 4.12.2020" url="/flowers/"/>
+                <Slider name="pandaClass/" text="panda 4.5.2020" url="/pandaClass/"/>
+                <Slider name="sunflowersClass/" text="sunflowers 4.26.2020" url="/sunflowersClass/"/>
+                <Slider name="rainbowSkyClass/" text="rainbow sky 5.24.2020" url="/rainbowSkyClass/"/>
+                <Slider name="fireflyClass/" text="firefly 5.17.2020" url="/fireflyClass/"/>
+                <Slider name="abstractFlowersClass/" text="abstract flowers 6.14.2020" url="/abstractFlowersClass/"/>
+                <Slider name="flowersClass/" text="flowers 4.12.2020" url="/flowersClass/"/>
                 <Slider name="birds/" text="birds 9.6.2020" url="/birds/"/>
                 <Slider name="lotusFlower/" text="lotus flower 7.12.2020" url="/lotusFlower/"/>
                 <Slider name="peacock/" text="peacock 7.26.2020" url="/peacock/"/>
@@ -45,7 +45,7 @@ export default function Teaching () {
                 <Slider name="koi/" text="koi 3.28.2021" url="/koi/"/>
                 <Slider name="springShade/" text="spring shade 3.21.2021" url="/springShade/"/>
                 <Slider name="keyToSummer/" text="key to summer 4.4.2021" url="/keyToSummer/"/>
-                <Slider name="littleBirdClass/" text="little bird 5.3.2020" url="/littleBird/"/>
+                <Slider name="littleBirdClass/" text="little bird 5.3.2020" url="/littleBirdClass/"/>
                 <Slider name="sunsetMountains/" text="sunset mountains 6.21.2020" url="/sunsetMountains/"/>
                 <Slider name="nightSky/" text="night sky 6.28.2020" url="/nightSky/"/>
                 <Slider name="cherryBlossom/" text="cherry blossom 5.10.2020" url="/cherryBlossom/"/>
